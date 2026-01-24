f7b518c3761a698c1b761f6153ac198644e4f7bd
Tim Milligan <voidzone@users.noreply.github.com>
2026-01-24 21:06:29 +0200

    Manually include LibRangeCheck

--- Variables.lua
+++ Variables.lua
@@ -232,7 +232,7 @@ Gnosis.tSoundChannels = {
 function Gnosis:StartupVariables()
 	local fCurTime = GetTime() * 1000;
 
-	self.ver = 5.28;
+	self.ver = 5.29;
 	self.optver = 4.62;
 	self.build = "@project-version@";
 	self.addonname = "Gnosis";
