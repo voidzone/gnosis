312e791275c875f398fa466611f4c84ab4afc09a
Tim Milligan <voidzone@users.noreply.github.com>
2026-01-25 04:26:00 +0200

    pkgmeta

--- Variables.lua
+++ Variables.lua
@@ -232,7 +232,7 @@ Gnosis.tSoundChannels = {
 function Gnosis:StartupVariables()
 	local fCurTime = GetTime() * 1000;
 
-	self.ver = 5.32;
+	self.ver = 5.33;
 	self.optver = 4.62;
 	self.build = "@project-version@";
 	self.addonname = "Gnosis";
